7f4fc73cf389cb7eeb2435306099d4e4e89c485a
Stephen Bannasch <stephen.bannasch@gmail.com>
2019-05-03 14:58:14 -0400

    Todos: adding and toggling completion state

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
+import Array
 import Browser
-import Html exposing (Html, div, h1, img, input, li, text, ul)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html exposing (Html, button, div, form, h1, img, input, li, text, ul)
+import Html.Attributes exposing (checked, class, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 
@@ -11,16 +12,22 @@ import Html.Events exposing (onInput)
 
 
 type alias Todo =
-    String
+    { description : String
+    , completed : Bool
+    }
+
+
+type alias TodoList =
+    List Todo
 
 
 type alias Model =
-    { todos : List Todo }
+    { todos : TodoList, pendingTodo : String }
 
 
 initialModel : Model
 initialModel =
-    { todos = [ "hello", "world" ] }
+    { todos = [], pendingTodo = "" }
 
 
 init : ( Model, Cmd Msg )
@@ -34,6 +41,27 @@ init =
 
 type Msg
     = NoOp
+    | AddTodo
+    | ToggleCompletedState Todo
+    | UpdatePendingTodo String
+
+
+toTodo : String -> Todo
+toTodo content =
+    { description = content, completed = False }
+
+
+toggleCompletedState : Todo -> TodoList -> TodoList
+toggleCompletedState todo todos =
+    List.map
+        (\t ->
+            if t.description == todo.description then
+                { t | completed = not t.completed }
+
+            else
+                t
+        )
+        todos
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -42,6 +70,21 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
+        AddTodo ->
+            ( { model | todos = toTodo model.pendingTodo :: model.todos, pendingTodo = "" }
+            , Cmd.none
+            )
+
+        UpdatePendingTodo content ->
+            ( { model | pendingTodo = content }
+            , Cmd.none
+            )
+
+        ToggleCompletedState todo ->
+            ( { model | todos = toggleCompletedState todo model.todos }
+            , Cmd.none
+            )
+
 
 
 ---- VIEW ----
@@ -49,15 +92,36 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Todos" ]
-        , ul [] <| List.map todoView model.todos
+    div [ class "flex-grid" ]
+        [ div [ class "col" ] []
+        , div [ class "col" ]
+            [ h1 [] [ text "Todos" ]
+            , form [ onSubmit AddTodo ]
+                [ input
+                    [ value model.pendingTodo
+                    , placeholder "Enter new Todo here ..."
+                    , onInput UpdatePendingTodo
+                    ]
+                    []
+                ]
+            , ul [] <| List.map todoView model.todos
+            ]
+        , div [ class "col" ] []
         ]
 
 
-todoView : String -> Html Msg
-todoView string =
-    li [] [ text string ]
+todoView : Todo -> Html Msg
+todoView todo =
+    li
+        []
+        [ input
+            [ type_ "checkbox"
+            , checked <| todo.completed
+            , onClick (ToggleCompletedState todo)
+            ]
+            []
+        , text todo.description
+        ]
 
 
 
